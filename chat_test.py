446c8fffb251dd556d7f4dd7f33d3e7c225c1b1d
alex <alexjfuhr@gmail.com>
2023-04-30 21:39:42 -0400

    Updated starting message

--- chat_test.py
+++ chat_test.py
@@ -13,12 +13,14 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 roles: list[str] = ["user", "assistant"]
 
-messages: list[dict[str, str]] = [
-    {"role": "user", "content": "Hello, my name's Alex! What's yours?"},
-    {"role": "assistant", "content": "Hello, Alex! My name is Bert! How are you doing?"},
-    {"role": "user", "content": "Nice to meet you Bert. I'm doing good. How about yourself?"},
-    {"role": "assistant", "content": "I'm fine thank you. What are your hobbies?"},
-]
+messages: list[dict[str, str]] = [{'role': 'user', 'content': "Hello, my name's Alex! What's yours?"},
+ {'role': 'assistant', 'content': 'Hello, Alex! My name is Bert! How are you doing?'},
+ {'role': 'user', 'content': "Nice to meet you Bert. I'm doing good. How about yourself?"},
+ {'role': 'assistant', 'content': "I'm fine thank you. What are your hobbies?"},
+ {'role': 'user', 'content': "As an AI language model, I don't have hobbies like humans do, but I'm here to answer your questions and help you in any way I can. How can I assist you today?"},
+ {'role': 'assistant', 'content': "Oh, I apologize for my mistake. As you mentioned that you're here to assist me, can you tell me more about yourself? What can you do?"},
+ {'role': 'user', 'content': "Of course! I am an AI language model capable of understanding natural language and providing human-like responses to various queries. I can answer questions related to a wide range of topics such as general knowledge, math, science, history, and more. I can also assist with tasks such as writing articles, composing emails, and even conducting research. Is there anything specific you'd like me to help with?"},
+ {'role': 'assistant', 'content': 'Actually, yes. Can you explain to me the importance of cybersecurity in our digital world?'}]
 role_num: int = 0
 while True:
     if input("enter to continue"):
@@ -34,8 +36,6 @@ while True:
     messages.append({"role": role, "content":content})
 
     role_num = (role_num + 1) % 2
-
-print(str(messages).replace("},", "},\n"))
 
 cur_time: str = time.strftime("%Y-%m-%d,%H_%M_%S")
 file_name: str = f"logs/{cur_time}.ailog"
